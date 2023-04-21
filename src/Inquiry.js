822488c842c0dc75943e9fbc49e49aa0c428d8b0
nichxlas98 <djflames50@gmail.com>
2023-04-21 11:02:52 -0500

    Added a confirmation notification to submissions.

--- src/Inquiry.js
+++ src/Inquiry.js
@@ -4,7 +4,8 @@ import './css/App.css';
 import './css/main.css';
 import './css/colors.css';
 import './css/nav.css';
-import './css/anims.css'
+import './css/anims.css';
+import './css/notif.css';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import React, { useState } from 'react';
 
@@ -13,7 +14,7 @@ function ContactForm() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [inquiry, setInquiry] = useState('');
-
+  const [showNotification, setShowNotification] = useState(false); // State for showing/hiding notification
   function handleSubmit(e) {
     e.preventDefault();
     console.log('Name:', name);
@@ -32,61 +33,76 @@ function ContactForm() {
                     .setColor("#aabbcc")
                     .setDescription(inquiry);
     Hook.send(msg);
+
+    // Reset the input field values to empty strings
+    setName('');
+    setEmail('');
+    setInquiry('');
+
+ 
+    // Show notification for 3 seconds
+    setShowNotification(true);
+    setTimeout(() => {
+      setShowNotification(false);
+    }, 3000);
   }
 
   return (
     <>
-    <div>
-      <Helmet>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
-      </Helmet>
-    </div>
-    
-    <div className="topnav">
-      <li>
-        <Link to="/">Home</Link>
-        <Link to="/projects">Projects</Link>
-        <Link className="active" to="/inquiries">Contact</Link>
-      </li>
-    </div>
-    <section className="dark">  
-      <p>&nbsp;</p>
-      <p>&nbsp;</p>
-      <h1 style={{ textAlign: 'center', color:"rgb(255, 255, 255)" }}>Inquiry Submission</h1>
-    <form onSubmit={handleSubmit}>
-        <label htmlFor="name" style= {{ color: "rgb(255, 255, 255)" }}>Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required />
+      <div>
+        <Helmet>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+        </Helmet>
+      </div>
 
-        <label htmlFor="email" style= {{ color: "rgb(255, 255, 255)" }}>Email:</label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required />
+      <div className="topnav">
+        <li>
+          <Link to="/">Home</Link>
+          <Link to="/projects">Projects</Link>
+          <Link className="active" to="/inquiries">Contact</Link>
+        </li>
+      </div>
+      <section className="dark">
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <h1 style={{ textAlign: 'center', color:"rgb(255, 255, 255)" }}>Inquiry Submission</h1>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="name" style= {{ color: "rgb(255, 255, 255)" }}>Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required />
 
-        <label htmlFor="inquiry" style= {{ color: "rgb(255, 255, 255)" }}>Inquiry:</label>
-        <textarea
-          id="inquiry"
-          name="inquiry"
-          value={inquiry}
-          onChange={(e) => setInquiry(e.target.value)}
-          required
-        ></textarea>
+          <label htmlFor="email" style= {{ color: "rgb(255, 255, 255)" }}>Email:</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required />
 
-        <input type="submit" value="Submit" />
-      </form>
+          <label htmlFor="inquiry" style= {{ color: "rgb(255, 255, 255)" }}>Inquiry:</label>
+          <textarea
+            id="inquiry"
+            name="inquiry"
+            value={inquiry}
+            onChange={(e) => setInquiry(e.target.value)}
+            required
+          ></textarea>
+
+          <input type="submit" value="Submit" />
+        </form>
+        {showNotification && (
+          <div className="notification">Your inquiry has been successfully submitted!</div>
+        )}
       </section>
-      </>
+    </>
   );
 }
 
